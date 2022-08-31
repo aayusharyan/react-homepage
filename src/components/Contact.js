4791e386c79cb182b2049a9b423be1036fb525a6
Aayush Sinha <aayush.aryan@me.com>
2022-08-31 22:34:44 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,10 +2,10 @@ const Contact = _ => {
   return (
     <section>
       <h3>🤙🏻 Contact Me</h3>
-      <a href="https://www.linkedin.com/in/aayushsinha/" target="_blank">LinkedIn</a>
-      <a href="https://github.com/aayusharyan" target="_blank">Github</a>
-      <a href="https://instagram.com/yush.dev" target="_blank">Instagram</a>
-      <a href="https://www.youtube.com/aayushsinha" target="_blank">YouTube</a>
+      <a href="https://www.linkedin.com/in/aayushsinha/" target="_blank" rel="noreferrer">LinkedIn</a>
+      <a href="https://github.com/aayusharyan" target="_blank" rel="noreferrer" >Github</a>
+      <a href="https://instagram.com/yush.dev" target="_blank" rel="noreferrer" >Instagram</a>
+      <a href="https://www.youtube.com/aayushsinha" target="_blank" rel="noreferrer" >YouTube</a>
     </section>
   )
 }
